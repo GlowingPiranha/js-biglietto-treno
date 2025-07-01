e0e414e0ef4d64e6109b2ad5850742ebf879840b
GlowingPiranha <ikimus01@gmail.com>
2025-07-01 17:41:43 +0200

    [end]

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-const km = parseInt(prompt('inserisci i chilometri che vuoi percorrere')) + '' + 'chilometri';
-const age = parseInt(prompt('inserisci la tua età')) + '' + 'anni';
-console.log(km)
-console.log(age)
+const km = parseInt(prompt('inserisci i chilometri che vuoi percorrere'));
+const age = parseInt(prompt('inserisci la tua età'));
+console.log(km + ' ' + 'chilometri')
+console.log(age, 'anni')
+
+if (age <= 17) {
+  const ticket = parseInt(km * 0.21);
+  const twentyp = ticket - ((ticket / 100) * 20);
+  const price = twentyp.toFixed(2);
+  console.log('il costo del bigletto è:' + ' ' + '€' + price)
+} else if (age >= 65) {
+  const ticket = parseInt(km * 0.21);
+  const twentyp = ticket - ((ticket / 100) * 40);
+  const price = twentyp.toFixed(2);
+  console.log('il costo del bigletto è:' + ' ' + '€' + price)
+} else {
+  const ticket = parseInt(km * 0.21);
+  const price = ticket.toFixed(2);
+  console.log('il costo del bigletto è:' + ' ' + '€' + price)
+}
