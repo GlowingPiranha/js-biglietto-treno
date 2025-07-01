d5b7a0fd8eee700c8350484398eae79c055b862f
GlowingPiranha <ikimus01@gmail.com>
2025-07-01 16:08:31 +0200

    [start]

---
+++ script.js
@@ -0,0 +1,4 @@
+const km = parseInt(prompt('inserisci i chilometri che vuoi percorrere')) + '' + 'chilometri';
+const age = parseInt(prompt('inserisci la tua età')) + '' + 'anni';
+console.log(km)
+console.log(age)
